58aa6890f6663ddd1f1f8f188a9aa6730434a78e
AbdallahSay <luka3122003@gmail.com>
2025-07-04 17:47:38 +0300

    Jugged Array

--- Demo4/Program.cs
+++ Demo4/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region jugged Array
+
+            //int[][] marks = new int[3][];
+            //marks[0] = new int[] { 10, 20, 30 };
+            //marks[1] = new int[] { 40, 50 };
+            //marks[2] = new int[] { 60 };
+            //for(int i =0; i < marks.Length; i++)
+            //{
+
+            //    for (int j = 0; j < marks[i].Length; j++)
+            //        Console.Write(marks[i][j] + "  ");
+            //            Console.WriteLine();
+            //}
+
+
+            #endregion
         }
     }
 }
